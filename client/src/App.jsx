a7e11fd668cec2e8a263451a442107dc346baf80
huzhuoliemma@gmail.com <huzhuoliemma@gmail.com>
2024-09-14 16:01:43 -0400

    setting up components and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage/HomePage';
+import QuizPage from './pages/QuizPage/QuizPage';
+import QuizResultPage from './pages/QuizResultPage/QuizResultPage';
+import AddQuizPage from './pages/AddQuizPage/AddQuizPage';
+import QuizAddedPage from './pages/QuizAddedPage/QuizAddedPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+import './App.scss'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <BrowserRouter>
+      <Header />
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/quiz" element={<QuizPage />} />
+              <Route path="/quiz/result" element={<QuizResultPage />} />
+              <Route path="/quiz/add" element={<AddQuizPage />} />
+              <Route path="/quiz/add/complete" element={<QuizAddedPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App
