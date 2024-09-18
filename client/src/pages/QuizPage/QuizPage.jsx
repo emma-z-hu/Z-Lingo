b251181699411ef05256c78772d7975fd2aeebce
huzhuoliemma@gmail.com <huzhuoliemma@gmail.com>
2024-09-18 12:14:51 -0400

    adding spinner between questions

--- client/src/pages/QuizPage/QuizPage.jsx
+++ client/src/pages/QuizPage/QuizPage.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import MultipleChoiceOption from '../../components/MultipleChoiceOption/MultipleChoiceOption';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
-import axios from 'axios';
-import './QuizPage.scss';
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import MultipleChoiceOption from "../../components/MultipleChoiceOption/MultipleChoiceOption";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Spinner from "../../components/Spinner/Spinner";
+import axios from "axios";
+import "./QuizPage.scss";
 
 const API_URL = import.meta.env.VITE_URL;
 
@@ -15,22 +16,22 @@ const QuizPage = () => {
   const [score, setScore] = useState(0);
   const [isCorrect, setIsCorrect] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isTransitioning, setIsTransitioning] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
-  
+
   const searchParams = new URLSearchParams(location.search);
-  const difficulty = searchParams.get('difficulty');
+  const difficulty = searchParams.get("difficulty");
 
   useEffect(() => {
     axios
       .get(`${API_URL}/api/quiz?difficulty=${difficulty}`)
       .then((response) => {
-        console.log('Fetched questions:', response.data.questions); // Debugging - TBD
         setQuestions(response.data.questions);
         setIsLoading(false);
       })
       .catch((error) => {
-        console.error('Failed to fetch quiz questions:', error);
+        console.error("Failed to fetch quiz questions:", error);
         setIsLoading(false);
       });
   }, [difficulty]);
@@ -47,6 +48,10 @@ const QuizPage = () => {
     }
 
     setTimeout(() => {
+      setIsTransitioning(true);
+    }, 500);
+
+    setTimeout(() => {
       if (currentQuestionIndex < questions.length - 1) {
         setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
       } else {
@@ -54,7 +59,8 @@ const QuizPage = () => {
       }
       setSelectedOption(null);
       setIsCorrect(null);
-    }, 2000); 
+      setIsTransitioning(false);
+    }, 2000);
   };
 
   if (isLoading) {
@@ -66,21 +72,28 @@ const QuizPage = () => {
   return (
     <div className="quiz-page">
       <Header difficulty={difficulty} />
-
-      <h1 className="quiz-page__question">{currentQuestion.question}</h1>
-      <div className="quiz-page__options">
-        {currentQuestion.options.map((option) => (
-          <MultipleChoiceOption
-            key={option}
-            label={option}
-            isSelected={selectedOption === option}
-            isCorrect={isCorrect !== null && option === currentQuestion.correctOption}
-            isIncorrect={isCorrect === false && option === selectedOption}
-            onClick={() => handleOptionSelect(option)}
-          />
-        ))}
-      </div>
-      <Footer currentQuestionIndex={currentQuestionIndex} totalQuestions={questions.length} />
+      {isTransitioning && <Spinner />}
+      <>
+        <h1 className="quiz-page__question">{currentQuestion.question}</h1>
+        <div className="quiz-page__options">
+          {currentQuestion.options.map((option) => (
+            <MultipleChoiceOption
+              key={option}
+              label={option}
+              isSelected={selectedOption === option}
+              isCorrect={
+                isCorrect !== null && option === currentQuestion.correctOption
+              }
+              isIncorrect={isCorrect === false && option === selectedOption}
+              onClick={() => handleOptionSelect(option)}
+            />
+          ))}
+        </div>
+      </>
+      <Footer
+        currentQuestionIndex={currentQuestionIndex}
+        totalQuestions={questions.length}
+      />
     </div>
   );
 };
